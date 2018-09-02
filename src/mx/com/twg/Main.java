9a295f7d06c9c80efcb307f304a57fefe5ca76e4
Noe <VaescoNoe@gmail.com>
2018-09-01 23:55:35 -0500

    Manejo de archivos

---
+++ src/mx/com/twg/Main.java
@@ -0,0 +1,14 @@
+package mx.com.twg;
+
+import mx.com.twg.manejoDatos.ManejoArchivos;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//ManejoArchivos.creacionArchivo();
+		ManejoArchivos.lecturaArchivo();
+		//ManejoArchivos.anexarInformacion();
+	}
+
+}
---
+++ src/mx/com/twg/manejoDatos/ManejoArchivos.java
@@ -0,0 +1,58 @@
+package mx.com.twg.manejoDatos;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ManejoArchivos {
+
+	public static void creacionArchivo(){
+		File archivo = new File("C:\\workspace\\ArchivosGenerados\\archivo.txt");
+		try{
+			PrintWriter salida = new PrintWriter(new FileWriter(archivo));
+			salida.close();
+		}catch(IOException ex){
+			ex.printStackTrace();
+		}
+		System.out.println("El archivo fue creado");
+	}
+	
+	public static void lecturaArchivo(){	
+		try{
+			File archivo = new File("C:\\workspace\\ArchivosGenerados\\archivoLectura.txt");
+			BufferedReader entrada = new BufferedReader(new FileReader(archivo));
+			String lectura = null;
+			lectura = entrada.readLine();
+			while(lectura!=null){
+				System.out.println("Lectura: "+lectura);
+				lectura = entrada.readLine();
+			}
+			entrada.close();
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException  ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	public static void anexarInformacion(){
+		File archivo = new File("C:\\workspace\\ArchivosGenerados\\archivo.txt");
+		try{
+			PrintWriter salida = new PrintWriter(new FileWriter(archivo,true));
+			String contenido = "De aquí se anexa información al archivo";
+			salida.println(contenido);
+			salida.println();
+			salida.println("Fin de enexar");
+			salida.close();
+		}catch(FileNotFoundException ex){
+			ex.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Se anexo la información correctamente");
+	}
+}
